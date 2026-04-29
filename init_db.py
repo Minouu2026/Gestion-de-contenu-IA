6484df323d6eed94d5de592e0d17099327c53ca6
Minouu2026 <minonyavs@gmail.com>
2026-04-29 22:20:09 +0300

    Update init_db.py

--- init_db.py
+++ init_db.py
@@ -3,8 +3,8 @@ from extensions import db
 from models.article import Article
 
 with app.app_context():
-    Article.query.delete()
-    db.session.commit()
+   db.create_all()
+    if Article.query.count() = 0:
     articles = [
         Article(nom="Robe élégante", prix=45000, categorie="Robe", description="Robe longue noir", image="robeElegante.JPG"),
         Article(nom="Sac cuir", prix=90000, categorie="Sac", description="sac en cuir marron", image="sacCuir.JPG"),
